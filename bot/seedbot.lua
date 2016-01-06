ac175019048778f285b1deee63d89acb1b6b960e
satanevil <satanevil150@gmail.com>
2016-01-07 01:19:50 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {142290354,0,tonumber(our_id)},--Sudo users
+    sudo_users = {142290354,163543039,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
